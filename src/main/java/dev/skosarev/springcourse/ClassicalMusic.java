b76ac771bdac6a74ac481acf9c7af87169177e20
Renat Skosarev <skosarrrev@gmail.com>
2022-10-29 02:20:37 +0500

    Lesson 1

---
+++ src/main/java/dev/skosarev/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package dev.skosarev.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/dev/skosarev/springcourse/Music.java
@@ -0,0 +1,5 @@
+package dev.skosarev.springcourse;
+
+public interface Music {
+    String getSong();
+}
