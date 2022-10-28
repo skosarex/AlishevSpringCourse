ba43a072d2ac00c0efe2882ab442b272273b4ed1
Renat Skosarev <skosarrrev@gmail.com>
2022-10-29 02:55:38 +0500

    Lesson 2

--- src/main/java/dev/skosarev/springcourse/MusicPlayer.java
+++ src/main/java/dev/skosarev/springcourse/MusicPlayer.java
@@ -2,10 +2,35 @@ package dev.skosarev.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
 
     // IoC
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
